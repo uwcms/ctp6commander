f853df6706c27ec560168b05ba0de46a6ec8c686
Evan Friis <ekfriis@gmail.com>
2013-09-03 08:26:22 -0500

    Script to download+setup virtualenv on the fly

---
+++ setup_venv.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Downloads virtualenv.py, sets up an environment, and installs dependencies.
+# Author: Evan K. Friis, UW
+
+VERSION=1.10.1
+
+curl -O https://pypi.python.org/packages/source/v/virtualenv/virtualenv-${VERSION}.tar.gz
+tar xzf virtualenv-${VERSION}.tar.gz
+
+python virtualenv-${VERSION}/virtualenv.py --system-site-packages env
+
+source env/bin/activate
+
+pip install argparse
+pip install flask
