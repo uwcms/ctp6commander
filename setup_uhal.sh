4df73bd195cde91b47faf2d515f9e0152eeec9d4
Evan Friis <ekfriis@gmail.com>
2013-09-03 10:12:50 -0500

    Add uhal env setup script

---
+++ setup_uhal.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+export CACTUS_HOME=cactus
+
+export PATH=$CACTUS_HOME/trunk/cactuscore/uhal/tests/bin:$PATH
+export LD_LIBRARY_PATH=$CACTUS_HOME/trunk/cactuscore/extern/boost/RPMBUILD/SOURCES/lib:$LD_LIBRARY_PATH
+export LD_LIBRARY_PATH=$CACTUS_HOME/trunk/cactuscore/extern/pugixml/RPMBUILD/SOURCES/lib:$LD_LIBRARY_PATH
+export LD_LIBRARY_PATH=$CACTUS_HOME/trunk/cactuscore/uhal/log/lib:$LD_LIBRARY_PATH
+export LD_LIBRARY_PATH=$CACTUS_HOME/trunk/cactuscore/uhal/grammars/lib:$LD_LIBRARY_PATH
+export LD_LIBRARY_PATH=$CACTUS_HOME/trunk/cactuscore/uhal/uhal/lib:$LD_LIBRARY_PATH
+export LD_LIBRARY_PATH=$CACTUS_HOME/trunk/cactuscore/uhal/tests/lib:$LD_LIBRARY_PATH
+
+export PYTHONPATH=$CACTUS_HOME/trunk/cactuscore/uhal/pycohal/pkg:$PYTHONPATH
+export PYTHONPATH=$CACTUS_HOME/trunk/cactuscore/uhal/gui:$PYTHONPATH
+export PYTHONPATH=$CACTUS_HOME/trunk/cactuscore/uhal/tools:$PYTHONPATH
